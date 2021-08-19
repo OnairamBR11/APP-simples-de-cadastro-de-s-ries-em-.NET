ce11c5711cefa3c27f8b090191b637c7a4af847c
OnairamBR11 <89218979+OnairamBR11@users.noreply.github.com>
2021-08-19 19:01:18 -0300

    Add files via upload

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using MariFlix.Series.Interfaces;
+
+namespace MariFlix.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+
+        private List<Serie> listaSerie = new List<Serie>();
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+    }
+}
